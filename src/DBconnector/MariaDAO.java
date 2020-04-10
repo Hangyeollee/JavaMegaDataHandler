abef24c9110a390197b1d9b5a2d9764632f6efa8
dkjang72 <dkjang72@gmail.com>
2020-04-10 13:38:58 +0900

    UPDATE 시 여러가지 컬럼 조건 줄수 있도록 기능 추가

--- src/DBconnector/MariaDAO.java
+++ src/DBconnector/MariaDAO.java
@@ -157,7 +157,40 @@ public class MariaDAO {
                             con.commit();
                         }
                     }else if(executeOption.equals("test")) System.out.println(sb.toString().replace("'null'","null"));
+                }
+                executeBatch(stmt);
+                con.commit();
+            } else if (info.getType().contains("updateWithCondition")){
+                int updateSeq = Integer.parseInt(info.getType().replace("updateWithCondition","").replace("(","").replace(")",""));
+                while (rs.next()) {
+                    StringJoiner updateData = new StringJoiner("','", "'", "'");
+                    for (int i=0; i < arrayLength; i++) {
+                        String data = rs.getString(array[i]);
+                        if(data != null && data.contains("'")) data = data.replace("'","\\'");
+                        updateData.add(data);
+                    }
+                    String [] updateArray = updateData.toString().split(",");
+                    StringBuilder sb = new StringBuilder();
+                    sb.append(String.format("UPDATE %s\nSET\n",targetTable));
 
+                    for(int i = updateSeq; i < targetColumnLength; i++){
+                        sb.append(String.format("%s = %s",targetColumn[i],updateArray[i]));
+                        if(i != arrayMaxIndex) sb.append(",\n");
+                        else sb.append("\n");
+                    }
+                    sb.append(String.format("WHERE %s = %s\n",targetColumn[0],updateArray[0]));
+                    for(int i = 1; i < updateSeq; i++){
+                        sb.append(String.format("AND %s = %s\n",targetColumn[i],updateArray[i]));
+                    }
+
+                    if(executeOption.equals("real")){
+                        stmt.addBatch(sb.toString().replace("'null'","null"));
+                        count++;
+                        if(count % batchSize == 0){
+                            executeBatch(stmt);
+                            con.commit();
+                        }
+                    }else if(executeOption.equals("test")) System.out.println(sb.toString().replace("'null'","null"));
                 }
                 executeBatch(stmt);
                 con.commit();
